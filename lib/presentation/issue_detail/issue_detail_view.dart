024348a657d1785c0012897ba6415580fb29c8af
Hossein Yousefi <hossein@identi.app>
2022-03-28 18:35:28 +0300

    set up the router

---
+++ lib/presentation/issue_detail/issue_detail_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IssueDetailView extends StatelessWidget {
+  final int number;
+
+  const IssueDetailView({
+    required this.number,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red);
+  }
+}
--- lib/presentation/issue_list/issue_list_view.dart
+++ lib/presentation/issue_list/issue_list_view.dart
@@ -5,6 +5,6 @@ class IssueListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(color: Colors.blue);
   }
 }
